b0a818bfda5aa4020c57edead6dc3b3cc208b135
neqbal <nooraineqbal@gmail.com>
2025-03-26 23:43:47 +0530

    .

--- frontend/src/pages/InstructorHome.jsx
+++ frontend/src/pages/InstructorHome.jsx
@@ -1,5 +1,55 @@
+import {
+  Avatar,
+  Dropdown,
+  DropdownDivider,
+  DropdownHeader,
+  DropdownItem,
+  Navbar,
+  NavbarBrand,
+  NavbarCollapse,
+  NavbarLink,
+  NavbarToggle,
+} from "flowbite-react";
+import reactLogo from "../assets/react.svg"
+
 function InstructorHome() {
-  return <div>InstructorHome</div>
+ return (
+    <Navbar fluid rounded>
+      <NavbarBrand href="https://flowbite-react.com">
+        <img src={reactLogo} className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">LMS Portal</span>
+      </NavbarBrand>
+      <div className="flex md:order-2">
+        <Dropdown
+          arrowIcon={false}
+          inline
+          label={
+            <Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded />
+          }
+        >
+          <DropdownHeader>
+            <span className="block text-sm">Bonnie Green</span>
+            <span className="block truncate text-sm font-medium">name@flowbite.com</span>
+          </DropdownHeader>
+          <DropdownItem>Dashboard</DropdownItem>
+          <DropdownItem>Settings</DropdownItem>
+          <DropdownItem>Earnings</DropdownItem>
+          <DropdownDivider />
+          <DropdownItem>Sign out</DropdownItem>
+        </Dropdown>
+        <NavbarToggle />
+      </div>
+      <NavbarCollapse>
+        <NavbarLink href="#" active>
+          Home
+        </NavbarLink>
+        <NavbarLink href="#">All Courses</NavbarLink>
+        <NavbarLink href="#">Enrolled Courses</NavbarLink>
+        <NavbarLink href="#">Quizes</NavbarLink>
+        <NavbarLink href="#">Submissions</NavbarLink>
+      </NavbarCollapse>
+    </Navbar>
+  );
 }
 
 export default InstructorHome
